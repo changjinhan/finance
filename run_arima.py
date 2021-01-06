6f37b07e09c20e728e02456d6f55ef8c79ccddc2
changjinhan <changjin9653@gmail.com>
2021-01-06 02:32:49 +0000

    S&P500 experiment

--- run_arima.py
+++ run_arima.py
@@ -17,7 +17,7 @@ from utils.metrics import normalized_quantile_loss
 import warnings
 warnings.filterwarnings("ignore")
 
-data_list = ['vol']
+data_list = ['sp500']
 
 for name in data_list:
     print('-'*30, name)
@@ -147,6 +147,56 @@ for name in data_list:
         for sym in symbol_name:
             ts = fill_missing_values(TimeSeries.from_dataframe(data[lambda x: (x.Symbol == sym) & (pd.to_datetime(x.date)<=pd.to_datetime('20190629'))], 'date', ['log_vol'], freq='B', fill_missing_dates=True), 'auto')
             train, val = ts.split_after(pd.Timestamp('20190621'))
+            train = train[-252:]
+            for model in models:
+                model.fit(train)
+                pred_val, conf_int = model.predict(len(val), return_conf_int=True)
+                # print(conf_int)
+                maes[str(model)].append(mae(pred_val, val))
+                mapes[str(model)].append(mape(pred_val, val))
+                rmses[str(model)].append(rmse(pred_val, val))
+                smapes[str(model)].append(smape(pred_val, val))
+
+                pred_val, val = _get_values_or_raise(pred_val, val, True)
+                pred_val = np.column_stack([conf_int[...,0], pred_val, conf_int[...,1]]) # quantile regression for q=[0.1, 0.5, 0.9]
+
+                # calculate quantile loss
+                q_losses = []
+                normalizer = np.sum(abs(val))
+                for i, q in enumerate(quantiles):
+                    # print(pred_val[..., i])
+                    errors = val - pred_val[..., i]
+                    q_losses.append((np.maximum((q - 1) * errors, q * errors)))
+                normalized_q_loss = 2 * np.sum(q_losses, axis=1) / normalizer
+                quantile_losses[str(model)].append(normalized_q_loss)
+
+        for model in models:
+            print(str(model) + " MAE: " , np.mean(maes[str(model)]))
+            print(str(model) + " MAPE: " , np.mean(mapes[str(model)]))
+            print(str(model) + " RMSE: " , np.mean(rmses[str(model)]))
+            print(str(model) + " SMAPE: " , np.mean(smapes[str(model)]))
+            print(str(model) + " Quantile Loss: ", np.mean(quantile_losses[str(model)], axis=0))
+
+    elif name == 'sp500':
+        symbol_name = data['Symbol'].unique()
+        maes = defaultdict(list)
+        mapes = defaultdict(list)
+        rmses = defaultdict(list)
+        smapes = defaultdict(list)
+        quantile_losses = defaultdict(list)
+
+        models = [
+                AutoARIMA(alpha=0.2), # for quantile 0.1, 0.9 
+                # Prophet(),
+                # ExponentialSmoothing(),
+                # Theta(),
+                # FFT()
+        ]
+        quantiles = [0.1, 0.5, 0.9]
+        for sym in symbol_name:
+            ts = fill_missing_values(TimeSeries.from_dataframe(data[lambda x: (x.Symbol == sym)], 'date', ['Close'], freq='B', fill_missing_dates=True), 'auto')
+            train, val = ts.split_after(pd.Timestamp('20201211'))
+            # print(len(val))
             train = train[-252:]
             for model in models:
                 model.fit(train)
