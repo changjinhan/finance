7685f1ce70385781c3ef1ab40a741f84d1fa1c58
changjinhan <changjin9653@gmail.com>
2021-01-27 07:53:16 +0000

    sparse transformer with alpha-entmax

---
+++ utils/sub_modules.py
@@ -0,0 +1,87 @@
+import math
+from typing import Dict, List, Tuple, Union
+
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+from entmax import sparsemax, entmax15
+
+class SparseScaledDotProductAttention(nn.Module):
+    def __init__(self, dropout: float = None, scale: bool = True, alpha: float = 1.5):
+        super(SparseScaledDotProductAttention, self).__init__()
+        if dropout is not None:
+            self.dropout = nn.Dropout(p=dropout)
+        else:
+            self.dropout = dropout
+        self.softmax = nn.Softmax(dim=2)
+        self.scale = scale
+        self.alpha = alpha
+
+    def forward(self, q, k, v, mask=None):
+        attn = torch.bmm(q, k.permute(0, 2, 1))  # query-key overlap
+
+        if self.scale:
+            dimension = torch.sqrt(torch.tensor(k.shape[-1]).to(torch.float32))
+            attn = attn / dimension
+
+        if mask is not None:
+            attn = attn.masked_fill(mask, -1e9)
+        
+        # sparse attention
+        if self.alpha == 1.5:
+            attn = entmax15(attn)
+        elif self.alpha == 2.0:
+            attn = sparsemax(attn)
+        else:
+            attn = self.softmax(attn)
+
+        if self.dropout is not None:
+            attn = self.dropout(attn)
+        output = torch.bmm(attn, v)
+        return output, attn
+
+
+class SparseMultiHeadAttention(nn.Module):
+    def __init__(self, n_head: int, d_model: int, dropout: float = 0.0, alpha: float = 1.5):
+        super(SparseMultiHeadAttention, self).__init__()
+
+        self.n_head = n_head
+        self.d_model = d_model
+        self.d_k = self.d_q = self.d_v = d_model // n_head
+        self.dropout = nn.Dropout(p=dropout)
+
+        self.v_layer = nn.Linear(self.d_model, self.d_v)
+        self.q_layers = nn.ModuleList([nn.Linear(self.d_model, self.d_q) for _ in range(self.n_head)])
+        self.k_layers = nn.ModuleList([nn.Linear(self.d_model, self.d_k) for _ in range(self.n_head)])
+        self.attention = SparseScaledDotProductAttention(alpha=alpha)
+        self.w_h = nn.Linear(self.d_v, self.d_model, bias=False)
+
+        self.init_weights()
+
+    def init_weights(self):
+        for name, p in self.named_parameters():
+            if "bias" not in name:
+                torch.nn.init.xavier_uniform_(p)
+            else:
+                torch.nn.init.zeros_(p)
+
+    def forward(self, q, k, v, mask=None) -> Tuple[torch.Tensor, torch.Tensor]:
+        heads = []
+        attns = []
+        vs = self.v_layer(v)
+        for i in range(self.n_head):
+            qs = self.q_layers[i](q)
+            ks = self.k_layers[i](k)
+            head, attn = self.attention(qs, ks, vs, mask)
+            head_dropout = self.dropout(head)
+            heads.append(head_dropout)
+            attns.append(attn)
+
+        head = torch.stack(heads, dim=2) if self.n_head > 1 else heads[0]
+        attn = torch.stack(attns, dim=2)
+
+        outputs = torch.mean(head, dim=2) if self.n_head > 1 else head
+        outputs = self.w_h(outputs)
+        outputs = self.dropout(outputs)
+
+        return outputs, attn
